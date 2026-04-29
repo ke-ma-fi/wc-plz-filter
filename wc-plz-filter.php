2aad2204f25f54690cd306165c2b84e27a5e9854
ke-ma-fi <146088050+ke-ma-fi@users.noreply.github.com>
2026-04-29 09:59:52 +0200

    just another commit

--- wc-plz-filter.php
+++ wc-plz-filter.php
@@ -4,7 +4,7 @@
  * Plugin Name:  WC PLZ-Filter
  * Plugin URI:   https://fischer.digitale-theke.com
  * Description:  PLZ-Popup mit drei Modi (Abholung, Lokale Lieferung, Postversand). Filtert Produkte dynamisch nach WooCommerce-Versandklassen und füllt den Checkout vor.
- * Version:      2.6.3
+ * Version:      2.6.4
  * Author:       Metzgerei Fischer
  * License:      Proprietary
  * License URI:  https://fischer.digitale-theke.com
@@ -24,7 +24,7 @@ defined( 'ABSPATH' ) || exit;
 
 final class WC_PLZ_Filter {
 
-    const VERSION = '2.6.3';
+    const VERSION = '2.6.4';
     const COOKIE  = 'wc_delivery_mode';
     const OPT     = 'wc_plz_filter_v2';
     const CACHE   = 'wc_plz_local_codes';
