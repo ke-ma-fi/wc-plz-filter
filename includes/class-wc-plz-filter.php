294ff2ac0af986aea927b20f747c2118813c14fc
ke-ma-fi <146088050+ke-ma-fi@users.noreply.github.com>
2026-07-22 19:14:40 +0200

    chore: update version number to 2.9.0 in plugin and class files

--- includes/class-wc-plz-filter.php
+++ includes/class-wc-plz-filter.php
@@ -5,7 +5,7 @@ final class WC_PLZ_Filter {
 
     use WC_PLZ_Singleton;
 
-    const VERSION         = '2.8.5';
+    const VERSION         = '2.9.0';
     const COOKIE          = 'wc_delivery_mode';
     const OPT             = 'wc_plz_filter_v2';
     const CACHE           = 'wc_plz_local_codes';
