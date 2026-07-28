0b7a3e6de864b7a05752acc72df80c0f219d62e0
ke-ma-fi <146088050+ke-ma-fi@users.noreply.github.com>
2026-07-28 14:01:55 +0200

    feat: update version to 2.10.5 and enhance Cart-Indicator styles for custom shop

--- includes/class-wc-plz-filter.php
+++ includes/class-wc-plz-filter.php
@@ -5,7 +5,7 @@ final class WC_PLZ_Filter {
 
     use WC_PLZ_Singleton;
 
-    const VERSION         = '2.10.4';
+    const VERSION         = '2.10.5';
     const COOKIE          = 'wc_delivery_mode';
     const OPT             = 'wc_plz_filter_v2';
     const CACHE           = 'wc_plz_local_codes';
